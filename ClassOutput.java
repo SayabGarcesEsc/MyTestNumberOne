f0953c97242d43d5e4e28424c98c39dd81127312
Sada Cruz Torres <sadacruztorres@gmail.com>
2023-07-12 15:40:30 -0500

    New branch and class file edited

---
+++ ClassOutput.java
@@ -0,0 +1,9 @@
+public class ClassOutput{
+	public String name;
+	public String age;
+	public String hobbies;
+	
+	public String myMethod(){
+		return "Inside ClassOuput class and outmeput brach.";
+	}
+}

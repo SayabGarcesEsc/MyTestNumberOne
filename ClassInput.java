b64a7f44f7f1528bcc367d437006e3409e9e312e
SayabGarcesEsc <tusolucionmates@gmail.com>
2023-07-17 18:16:34 -0500

    Funcionality added. Class Main added.

--- ClassInput.java
+++ ClassInput.java
@@ -2,4 +2,14 @@ public class ClassInput{
 	private String name;
 	private String age;
 	private String hobbies;
+
+	public void inputMethod(){
+		System.out.println("Class Input method.");
+	}
+	public String getName(){ 
+		return this.name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
 }
